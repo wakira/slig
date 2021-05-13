2c995e6e23d73990c855fadb6353bae9cd25f1b7
Sheng Wang <sheng@a64.work>
2021-05-13 20:23:32 +0900

    add reader-writer lock support

--- slig.py
+++ slig.py
@@ -100,15 +100,15 @@ class ClonedGitRepo:
             print(e, file=sys.stderr)
             sys.exit(1)
 
-    def add_lock(self, lock_name):
+    def add_lock(self, lock_name, lock_type):
         config = configparser.ConfigParser()
         try:
             config.read(self.path / REPO_CONFIG_FILENAME)
             if lock_name in config['locks']:
-                # FIXME: what to do if lock_name already exists?
-                pass
+                print("Lock {} already exists".format(lock_name), file=sys.stderr)
+                sys.exit(1)
             else:
-                config['locks'][lock_name] = 'simple'  # TODO: currently only simple lock is supported
+                config['locks'][lock_name] = lock_type
         except:
             print("Cannot parse {} in target repository".format(REPO_CONFIG_FILENAME), file=sys.stderr)
 
@@ -117,8 +117,7 @@ class ClonedGitRepo:
                 config.write(file)
 
             self._call_git_command_raise(["add", REPO_CONFIG_FILENAME])
-            # TODO: currently only simple lock is supported
-            self._call_git_command_raise(["commit", "-m", "add {} lock: {}".format("simple", lock_name)])
+            self._call_git_command_raise(["commit", "-m", "add {} lock: {}".format(lock_type, lock_name)])
             self._call_git_command_raise(["push"])
         except GitError as e:
             print(e, file=sys.stderr)
@@ -149,7 +148,13 @@ class ClonedGitRepo:
             print(e, file=sys.stderr)
             sys.exit(1)
 
-    def acquire(self, lock_name, comment=None):
+    def _lock_acquired(self, lock_name):
+        return lock_name in map(lambda x: x.name, pathlib.Path(self.path).iterdir())
+
+    def _num_read_lock_acquired(self, lock_name):
+        return len(list(filter(lambda x: x.name.startswith(lock_name + ".read."), pathlib.Path(self.path).iterdir())))
+
+    def acquire(self, lock_name, comment=None, rw_action=None):
         config = configparser.ConfigParser()
         try:
             config.read(self.path / REPO_CONFIG_FILENAME)
@@ -157,35 +162,67 @@ class ClonedGitRepo:
                 print("Lock {} doesn't exist in repository".format(lock_name), file=sys.stderr)
                 sys.exit(1)
 
+            lock_type = config['locks'][lock_name]
+
             # check if lock is in use
-            if lock_name in map(lambda x: x.name, pathlib.Path(self.path).iterdir()):
+            if lock_type == 'simple' and self._lock_acquired(lock_name):
                 print("Lock {} is currently acquired."
                         .format(lock_name), file=sys.stderr)
                 sys.exit(1)
-            else:
-                # try to acqurie the lock
+            elif lock_type == 'readwrite' and rw_action == 'read':
+                if self._lock_acquired(lock_name):
+                    print("Write lock of {} is currently acquired."
+                            .format(lock_name), file=sys.stderr)
+                    sys.exit(1)
+            elif lock_type == 'readwrite' and rw_action == 'write':
+                if self._lock_acquired(lock_name):
+                    print("Write lock of {} is currently acquired."
+                            .format(lock_name), file=sys.stderr)
+                    sys.exit(1)
+                if self._num_read_lock_acquired(lock_name) != 0:
+                    print("Read locks of {} are currently acquired."
+                            .format(lock_name), file=sys.stderr)
+                    sys.exit(1)
+
+            # try to acqurie the lock
+            # when acquiring READ lock, also write "READ" into lock1
+            # when acquiring WRITE lock, write uuid like usual
+            unique_token = str(u.uuid4())
+
+            if lock_type == 'simple' or rw_action == 'write':
                 with open(self.path / lock_name, "w") as lock_file:
-                    unique_token = str(u.uuid4())
                     lock_file.write(unique_token)
                     self._call_git_command_raise(["add", lock_name])
-                    if comment:
-                        self._call_git_command_raise(["commit", "-m", "acquire lock: {}\n\n{}".format(lock_name, comment)])
-                    else:
-                        self._call_git_command_raise(["commit", "-m", "acquire lock: {}".format(lock_name)])
+            elif lock_type == 'readwrite' and rw_action == 'read':
+                read_lock_name = lock_name + '.read.' + unique_token
+                with open(self.path / read_lock_name, "w") as read_lock_file:
+                    read_lock_file.write(unique_token)
+                    self._call_git_command_raise(["add", read_lock_name])
+                with open(self.path / lock_name, "w") as lock_file:
+                    lock_file.write("READ")
+                    self._call_git_command_raise(["add", lock_name])
+            else:
+                raise RuntimeError("Impossible branch, possibly bug in coding")
 
-                    if self._sync_check_conflict():
-                        return unique_token
-                    else:
-                        print("Lock {} might be currently acquired."
-                              .format(lock_name), file=sys.stderr)
-                        sys.exit(1)
+            if comment:
+                self._call_git_command_raise(["commit", "-m", "acquire lock: {}\n\n{}".format(lock_name, comment)])
+            else:
+                self._call_git_command_raise(["commit", "-m", "acquire lock: {}".format(lock_name)])
 
-                # TODO: control should never reach here?
-                raise RuntimeError("Error acquiring lock")
+            if self._sync_check_conflict():
+                return unique_token
+            else:
+                print("Lock {} might be currently acquired."
+                        .format(lock_name), file=sys.stderr)
+                sys.exit(1)
         except GitError as e:
             print(e, file=sys.stderr)
             sys.exit(1)
 
+    # when releasing read, remove lock1.read.{uuid}, if no other read locks acquired, remove lock1 as well
+    # when releasing write, remove as usual
+    # force releasing a read-write lock is problematic, as we don't know exactly which lock to release
+    # we simply emit an error. users should solve it manually
     def release(self, lock_name, uuid=None):
         config = configparser.ConfigParser()
         try:
@@ -194,21 +231,45 @@ class ClonedGitRepo:
                 print("Lock {} doesn't exist in repository".format(lock_name), file=sys.stderr)
                 sys.exit(1)
 
+            lock_type = config['locks'][lock_name]
+
             # check if lock is in use
             if lock_name not in map(lambda x: x.name, pathlib.Path(self.path).iterdir()):
                 print("Lock {} is currently not acquired."
                         .format(lock_name), file=sys.stderr)
                 sys.exit(1)
             else:
+                release_read_lock = None
                 if uuid:
                     with open(self.path / lock_name, "r") as lock_file:
                         old_uuid = lock_file.readline()
-                        if uuid != old_uuid:
+                        if old_uuid == 'READ':
+                            # in this case uuid should be in the reader lock
+                            # check the existence of lock_name.read.{uuid}
+                            read_lock_name = lock_name + '.read.' + uuid
+                            if read_lock_name not in map(lambda x: x.name, pathlib.Path(self.path).iterdir()):
+                                print("No reader lock in uuid: {}".format(uuid))
+                                sys.exit(1)
+                            release_read_lock = read_lock_name
+                        elif uuid != old_uuid:
                             print("Cannot release lock {}, acquired by another uuid: {}".format(lock_name,uuid),
                                   file=sys.stderr)
                             sys.exit(1)
-                self._call_git_command_raise(["rm", lock_name])
-                self._call_git_command_raise(["commit", "-m", "release lock: {}".format(lock_name)])
+                elif lock_type == 'readwrite':
+                    print("Read-write lock {} cannot be force-released, try doing it manually".format(lock_name),
+                            file=sys.stderr)
+                    sys.exit(1)
+
+                if release_read_lock:
+                    self._call_git_command_raise(["rm", release_read_lock])
+                    # if all read lock is removed
+                    if self._num_read_lock_acquired(lock_name) == 0:
+                        self._call_git_command_raise(["rm", lock_name])
+                    self._call_git_command_raise(["commit", "-m", "release read lock: {} in uuid: {}"
+                                                  .format(release_read_lock, uuid)])
+                else:
+                    self._call_git_command_raise(["rm", lock_name])
+                    self._call_git_command_raise(["commit", "-m", "release lock: {}".format(lock_name)])
                 if not self._sync_check_conflict():
                     print("Lock {} cannot be released."
                           .format(lock_name), file=sys.stderr)
@@ -260,6 +321,8 @@ def setup_locks_subparser(subparsers):
     locks_add = locks_subparsers.add_parser("add", help="add a lock")
     locks_add.set_defaults(locks_action="add")
     locks_add.add_argument("lock_name", help="name of lock to add")
+    locks_add.add_argument("--simple", action="store_true", help="simple locking (default)")
+    locks_add.add_argument("--readwrite", action="store_true", help="reader-writer locking")
 
     locks_delete = locks_subparsers.add_parser("delete", help="delete a lock")
     locks_delete.set_defaults(locks_action="delete")
@@ -270,6 +333,8 @@ def setup_acquire_subparser(subparsers):
     parser_acquire.set_defaults(action="acquire")
     parser_acquire.add_argument("lock_name", help="name of lock")
     parser_acquire.add_argument("-c", "--comment", dest="comment", help="comments to write into commit message")
+    parser_acquire.add_argument("--read", action="store_true", help="comments to write into commit message")
+    parser_acquire.add_argument("--write", action="store_true", help="comments to write into commit message")
 
 def setup_release_subparser(subparsers):
     parser_release = subparsers.add_parser("release", help="release a lock")
@@ -286,19 +351,34 @@ if __name__ == "__main__":
     remote = env_get_repo()
     git_options = env_get_git_options()
 
+    # TODO: refactor arg handling
     if args.action == "repo" and args.repo_action == 'init':
         repo = ClonedGitRepo(remote, git_options)
         repo.initialize()
     elif args.action == "locks" and args.locks_action == 'add' and args.lock_name:
         repo = ClonedGitRepo(remote, git_options)
-        repo.add_lock(args.lock_name)
+        if args.readwrite and not args.simple:
+            repo.add_lock(args.lock_name, lock_type="readwrite")
+        elif args.readwrite and args.simple:
+            parser.print_help()
+        else:
+            repo.add_lock(args.lock_name, lock_type="simple")
     elif args.action == "locks" and args.locks_action == 'delete' and args.lock_name:
         repo = ClonedGitRepo(remote, git_options)
         repo.remove_lock(args.lock_name)
     elif args.action == "acquire" and args.lock_name:
         repo = ClonedGitRepo(remote, git_options)
-        uuid = repo.acquire(args.lock_name, args.comment)
-        print(uuid)  # print uuid of the lock to stdout
+        if args.read and not args.write:
+            uuid = repo.acquire(args.lock_name, args.comment, rw_action="read")
+            print(uuid)  # print uuid of the lock to stdout
+        elif args.write and not args.read:
+            uuid = repo.acquire(args.lock_name, args.comment, rw_action="write")
+            print(uuid)  # print uuid of the lock to stdout
+        elif args.write and args.read:
+            parser.print_help()
+        else:
+            uuid = repo.acquire(args.lock_name, args.comment)
+            print(uuid)  # print uuid of the lock to stdout
     elif args.action == "release" and args.lock_name and args.uuid and not args.force:
         repo = ClonedGitRepo(remote, git_options)
         uuid = repo.release(args.lock_name, args.uuid)
